365ccf7717b7f459bb0776dbcbc80cef6c871f90
Joe Ryan <herrad.koe@gmail.com>
2022-05-31 18:58:51 +0100

    start controls

--- web/routes/character.js
+++ web/routes/character.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var createLiveScanner = require('../../analyse/liveScan');
+const createScanner = require('../../analyse/scan');
 
 function createRouter(path, socketServer) {
   var router = express.Router();
@@ -9,6 +10,10 @@ function createRouter(path, socketServer) {
     createLiveScanner(socketServer).scan({}, req.params.characterName, path);
     res.render('character')
   });
+  router.get('/:characterName/scan', (req, res) => {
+    createScanner(socketServer).scan({}, req.params.characterName, path);
+    setTimeout(() => res.json(data), 500);
+  })
 
   return router;
 }
